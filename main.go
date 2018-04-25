eb016b09f03e1dd0fb5b215fc733c2a555653a25
Nick Miyake <nmiyake@users.noreply.github.com>
2018-04-25 15:49:58 -0700

    Update cobra CLI version (#12)

--- main.go
+++ main.go
@@ -32,7 +32,6 @@ func main() {
 
 func checkMain(osArgs []string) int {
 	os.Args = osArgs
-	var debugFlagVal bool
 	rootCmd := checker.AssetRootCmd(creator.Deadcode(), config.UpgradeConfig, "run deadcode check")
-	return cobracli.ExecuteWithDefaultParamsWithVersion(rootCmd, &debugFlagVal, "")
+	return cobracli.ExecuteWithDefaultParams(rootCmd)
 }
